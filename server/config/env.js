7aff5ad6bdc382baa34d8bcb24b75052626de405
Teoman Soygul <teo@soygul.com>
2014-02-19 17:19:56 +0100

    more fixes for env

--- server/config/env.js
+++ server/config/env.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var path = require('path');
+var path = require('path'),
+    _ = require('lodash');
 
 /**
  * Environment variables and application configuration.
@@ -16,59 +17,67 @@ var env = {
   },
   mongo: {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/koan'
+  },
+  setEnv: function (e) {
+    env = _.extend(env, conf[e]);
   }
 };
 
-var dev = {
-  app: {
-    env: 'development'
-  },
-  mongo: {
-    uri: 'mongodb://localhost:27017/koan-dev'
-  }
-};
-
-var test = {
-  app: {
-    port: 3001,
-    env: 'test'
-  },
-  mongo: {
-    uri: 'mongodb://localhost:27017/koan-test'
-  }
-};
-
-var prod = {
-  app: {
-    env: 'production',
-    masterToken: null
-  },
-  passport: {
-    facebook: {
-      clientID: 'APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/auth/facebook/callback'
+var conf = {
+  dev: {
+    app: {
+      env: 'development'
     },
-    twitter: {
-      clientID: 'CONSUMER_KEY',
-      clientSecret: 'CONSUMER_SECRET',
-      callbackURL: 'http://localhost:3000/auth/twitter/callback'
-    },
-    google: {
-      clientID: 'APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/auth/google/callback'
-    },
-  }
-};
-
-var staging = {
-  app: {
-    port: 3002,
-    env: 'staging',
-    masterToken: null
+    mongo: {
+      uri: 'mongodb://localhost:27017/koan-dev'
+    }
   },
-  mongo: {
-    uri: 'mongodb://localhost:27017/koan-staging'
+
+  test: {
+    app: {
+      port: 3001,
+      env: 'test'
+    },
+    mongo: {
+      uri: 'mongodb://localhost:27017/koan-test'
+    }
+  },
+
+  prod: {
+    app: {
+      env: 'production',
+      masterToken: null
+    },
+    mongo: {
+      uri: 'mongodb://localhost:27017/koan'
+    },
+    passport: {
+      facebook: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+      },
+      twitter: {
+        clientID: 'CONSUMER_KEY',
+        clientSecret: 'CONSUMER_SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+      },
+      google: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+      }
+    }
+  },
+
+  staging: {
+    app: {
+      port: 3002,
+      env: 'staging',
+      masterToken: null
+    },
+    mongo: {
+      uri: 'mongodb://localhost:27017/koan-staging'
+    }
   }
 };
