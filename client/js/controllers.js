c6c91eb5b60f375c3afd749b8211676457d668d7
Teoman Soygul <teo@soygul.com>
2014-03-04 17:31:05 +0100

    added client side server app ctrls

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -8,21 +8,65 @@ angular.module('koan.controllers', []).
     })
     .controller('MyCtrl2',function () {
 
-    }).controller('UserCtrl', function ($scope, $http, $window) {
+    }).controller('UserCtrl',function ($scope, $http, $window) {
       $scope.user = {username: 'john.doe', password: 'foobar'};
+      $scope.isAuthenticated = false;
+      $scope.welcome = '';
       $scope.message = '';
+
       $scope.submit = function () {
-        $http.post('/authenticate', $scope.user)
+        $http
+            .post('/authenticate', $scope.user)
             .success(function (data, status, headers, config) {
               $window.sessionStorage.token = data.token;
-              $scope.message = 'Welcome';
+              $scope.isAuthenticated = true;
+              var encodedProfile = data.token.split('.')[1];
+              var profile = JSON.parse(atob(encodedProfile));
+              $scope.welcome = 'Welcome ' + profile.first_name + ' ' + profile.last_name;
             })
             .error(function (data, status, headers, config) {
               // Erase the token if the user fails to log in
               delete $window.sessionStorage.token;
+              $scope.isAuthenticated = false;
 
               // Handle login errors here
-              $scope.message = 'Error: Invalid user or password';
+              $scope.error = 'Error: Invalid user or password';
+              $scope.welcome = '';
             });
       };
+
+      $scope.logout = function () {
+        $scope.welcome = '';
+        $scope.message = '';
+        $scope.isAuthenticated = false;
+        delete $window.sessionStorage.token;
+      };
+
+      $scope.callRestricted = function () {
+        $http({url: '/api/restricted', method: 'GET'})
+            .success(function (data, status, headers, config) {
+              $scope.message = $scope.message + ' ' + data.name; // Should log 'foo'
+            })
+            .error(function (data, status, headers, config) {
+              alert(data);
+            });
+      };
+    }).factory('authInterceptor',function ($rootScope, $q, $window) {
+      return {
+        request: function (config) {
+          config.headers = config.headers || {};
+          if ($window.sessionStorage.token) {
+            config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+          }
+          return config;
+        },
+        responseError: function (rejection) {
+          if (rejection.status === 401) {
+            // handle the case where the user is not authenticated
+          }
+          return $q.reject(rejection);
+        }
+      };
+    }).config(function ($httpProvider) {
+      $httpProvider.interceptors.push('authInterceptor');
     });
