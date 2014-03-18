8ddad43fa4674f27dd05b2fd003488ca9847b3d7
Teoman Soygul <teo@soygul.com>
2014-03-18 19:09:26 +0100

    add ctrl comments

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -3,8 +3,11 @@
 /* Controllers */
 
 angular.module('koan.controllers', [])
+
     .controller('layout', function ($rootScope, $window) {
-      // layout controller is always invoked once, regardless of the given route, to prepare the UI layout variables (like user profile image, display name, online status, etc.)
+      /**
+       * layout controller is always invoked once, regardless of the given route, to prepare the UI layout variables (like user profile image, display name, online status, etc.)
+       */
       var layout = $rootScope.layout = $rootScope.layout || {
         active: {},
         user: JSON.parse($window.sessionStorage.user || $window.localStorage.user),
@@ -17,10 +20,17 @@ angular.module('koan.controllers', [])
         }
       };
     })
-    .controller('home', function () {
 
+    .controller('home', function () {
+      /**
+       * home controller simply lists all the posts from everyone on the front page
+       */
     })
+
     .controller('profile', function ($scope, $http, $window) {
+      /**
+       * profile controller gives the user the means to view/edit their public profile info
+       */
       $scope.callRestricted = function () {
         $http({url: '/api/users', method: 'GET'})
             .success(function (data, status, headers, config) {
