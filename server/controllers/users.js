75ec3038cff582576ae16511ce83edaf100f7506
Teoman Soygul <teo@soygul.com>
2014-03-19 16:35:22 +0100

    add user img

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var route = require('koa-route'),
-    parse = require('co-body');
+    parse = require('co-body'),
+    mongo = require('./server/config/mongo');
 
 /**
  * Register koa routes.
  */
 exports.init = function (app) {
+  app.get(route.get('/users/:id/picture', getPicture));
   app.use(route.get('/api/users', list));
   app.use(route.get('/api/users/post/:id', show));
   app.use(route.post('/api/users/post', create));
@@ -32,4 +34,14 @@ function *create() {
   user.created_at = new Date();
   user.id = id;
   this.redirect('/');
+}
+
+function *getPicture(id) {
+  id = parseInt(id);
+  var user = yield mongo.findOne({_id: id}, {photo: 1});
+  if (user) {
+    var img = new Buffer(user.photo, 'base64');
+    this.set('Content-Type', 'image/jpeg');
+    this.body = img;
+  }
 }
