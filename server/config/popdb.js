c27730858862c32501284d8f06c157e8131bd9d8
Teoman Soygul <teo@soygul.com>
2014-03-27 12:45:48 +0100

    subtle fixes

--- server/config/popdb.js
+++ server/config/popdb.js
@@ -51,36 +51,37 @@ var users = [
 ];
 
 
-Date.prototype.subtractHours = function (h) {
-  return new Date(new Date(this).setHours(this.getHours() - h));
-};
 var now = new Date();
+function getTime(h) {
+  return new Date(new Date(now).setHours(now.getHours() + h));
+}
+
 var posts = [
-  {
-    _id: new ObjectID(),
-    from: {_id: 2, name: 'Chuck Norris', picture: 'api/users/2/picture'},
-    message: 'Hello world! This is a sample post... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat neque dui, vel fermentum arcu egestas vitae. Curabitur tincidunt tristique tristique. Nunc velit sapien, laoreet non enim vel, vestibulum blandit magna. Vivamus pharetra tempus mi, id ullamcorper dolor adipiscing at. Aliquam sed malesuada sapien. Sed enim nisi, rhoncus nec sollicitudin quis, mattis sed eros. Etiam luctus aliquam tristique. Aenean condimentum justo arcu, sit amet vestibulum neque suscipit sit amet. Nunc mattis enim eget turpis semper bibendum. Quisque blandit diam tincidunt lacus malesuada, vel faucibus risus hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque viverra tellus bibendum, elementum urna ultricies, elementum enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam nunc turpis, commodo id ipsum ac, rhoncus auctor ligula. Proin in nulla in lorem commodo semper. Aenean sodales turpis vitae tempor sollicitudin.',
-    createdTime: now.subtractHours(28)
-  },
   {
     _id: new ObjectID(),
     from: {_id: 1, name: 'Teoman Soygul', picture: 'api/users/1/picture'},
     message: 'Hi guys, I\'m traveling to Bolivia for the weekend!',
-    createdTime: now.subtractHours(97),
-    updatedTime: now.subtractHours(24),
+    createdTime: getTime(-97),
+    updatedTime: getTime(-24),
     comments: [
       {
         _id: new ObjectID(),
         from: {_id: 2, name: 'Chuck Norris', picture: 'api/users/2/picture'},
-        createdTime: now.subtractHours(26),
+        createdTime: getTime(-26),
         message: 'Don\'t forget to bring back an iguana:)'
       },
       {
         _id: new ObjectID(),
         from: {_id: 1, name: 'Teoman Soygul', picture: 'api/users/1/picture'},
-        createdTime: now.subtractHours(24),
+        createdTime: getTime(-24),
         message: 'I will!.'
       }
     ]
+  },
+  {
+    _id: new ObjectID(),
+    from: {_id: 2, name: 'Chuck Norris', picture: 'api/users/2/picture'},
+    message: 'Hello world! This is a sample post... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat neque dui, vel fermentum arcu egestas vitae. Curabitur tincidunt tristique tristique. Nunc velit sapien, laoreet non enim vel, vestibulum blandit magna. Vivamus pharetra tempus mi, id ullamcorper dolor adipiscing at. Aliquam sed malesuada sapien. Sed enim nisi, rhoncus nec sollicitudin quis, mattis sed eros. Etiam luctus aliquam tristique. Aenean condimentum justo arcu, sit amet vestibulum neque suscipit sit amet. Nunc mattis enim eget turpis semper bibendum. Quisque blandit diam tincidunt lacus malesuada, vel faucibus risus hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque viverra tellus bibendum, elementum urna ultricies, elementum enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam nunc turpis, commodo id ipsum ac, rhoncus auctor ligula. Proin in nulla in lorem commodo semper. Aenean sodales turpis vitae tempor sollicitudin.',
+    createdTime: getTime(-28)
   }
 ];
