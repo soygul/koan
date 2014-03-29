a32a35354282fbf184b1882870589eca522767f2
Teoman Soygul <teo@soygul.com>
2014-03-29 10:17:14 +0100

    update api service

--- client/js/services.js
+++ client/js/services.js
@@ -14,38 +14,46 @@ angular.module('koan.services', [])
           api = {events: {}};
 
       // initiate the websocket connection to the host
-      api.init = function () {
-        var ws = api.ws = new WebSocket(wsHost + '?access_token=' + token),
-            wsEvents = api.events.ws = {};
+      var ws = api.ws = new WebSocket(wsHost + '?access_token=' + token),
+          wsEvents = api.events.ws = {};
 
-        // create event objects for controllers to subscribe
-        wsEvents.connected = event();
-        wsEvents.disconnected = event();
-        wsEvents.postCreated = event();
-        wsEvents.commentCreated = event();
+      // utilize jQuery's callbacks as an event system
+      function event() {
+        var callbacks = $.Callbacks();
+        return {
+          subscribe: callbacks.add,
+          unsubscribe: callbacks.remove,
+          publish: callbacks.fire /* todo: mod this to be publish($scope, fn) to support auto unsubscribe on ctrl destruction w/ $scope.$on('$destroy', fn); */
+        };
+      }
 
-        // add event listeners to the websocket event and publish them to subscribers
-        ws.addEventListener('open', function () {
-          wsEvents.connected.publish.apply(this, arguments);
-          $rootScope.$apply();
-        });
+      // create event objects for controllers to subscribe
+      wsEvents.connected = event();
+      wsEvents.disconnected = event();
+      wsEvents.postCreated = event();
+      wsEvents.commentCreated = event();
 
-        ws.addEventListener('close', function () {
-          wsEvents.disconnected.publish.apply(this, arguments);
-          $rootScope.$apply();
-        });
+      // add event listeners to the websocket event and publish them to subscribers
+      ws.addEventListener('open', function () {
+        wsEvents.connected.publish.apply(this, arguments);
+        $rootScope.$apply();
+      });
 
-        ws.addEventListener('message', function (event /* websocket event object */) {
-          var data = JSON.parse(event.data /* rpc event object (data) */);
-          if (!data.method) {
-            throw 'Malformed event data received through WebSocket. Received event data object was: ' + data;
-          } else if (!wsEvents[data.method]) {
-            throw 'Undefined event type received through WebSocket. Received event data object was: ' + data;
-          }
-          wsEvents[data.method].publish(data.params);
-          $rootScope.$apply();
-        });
-      };
+      ws.addEventListener('close', function () {
+        wsEvents.disconnected.publish.apply(this, arguments);
+        $rootScope.$apply();
+      });
+
+      ws.addEventListener('message', function (event /* websocket event object */) {
+        var data = JSON.parse(event.data /* rpc event object (data) */);
+        if (!data.method) {
+          throw 'Malformed event data received through WebSocket. Received event data object was: ' + data;
+        } else if (!wsEvents[data.method]) {
+          throw 'Undefined event type received through WebSocket. Received event data object was: ' + data;
+        }
+        wsEvents[data.method].publish(data.params);
+        $rootScope.$apply();
+      });
 
       // api endpoints
       api.posts = {
@@ -61,16 +69,6 @@ angular.module('koan.services', [])
           }
         }
       };
-
-      // utilize jQuery's callbacks as an event system
-      function event() {
-        var callbacks = $.Callbacks();
-        return {
-          subscribe: callbacks.add,
-          unsubscribe: callbacks.remove,
-          publish: callbacks.fire
-        };
-      }
 
       return api;
     });
