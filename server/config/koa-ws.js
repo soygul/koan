6c91ff09a940f7965ee1583debc2b3a79e70624d
Teoman Soygul <teo@soygul.com>
2014-03-30 03:05:59 +0200

    update note

--- server/config/koa-ws.js
+++ server/config/koa-ws.js
@@ -5,3 +5,10 @@
  */
 
 var ws = require('./ws');
+
+module.exports = function (opts) {
+  return function *ws(next) {
+    // 1. check if this response is for a json-rpc request (with a proper id) through websocket
+    // 2. check if we need to broadcast anything to anyone even if it's not a direct response for the request
+  };
+}
