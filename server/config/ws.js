0e5a1cae1e61aee026634bd28b0fc67aef65d322
Teoman Soygul <teo@soygul.com>
2014-03-31 15:30:07 +0200

    add more exp features

--- server/config/ws.js
+++ server/config/ws.js
@@ -128,6 +128,8 @@ exports.commentCreated = function (comment) {
 
 exports.middleware = function () {
 
+  // exports.wss._server._events.request({websocket: true, url: '/', headers: {}, socket: {writable: false}}, {setHeader: function () {}, once: function () {}});
+
   return function *wsMiddleware(next) {
     // 1. check if this response is for a json-rpc request (with a proper id) through websocket, or a normal http request
     // 2. check if we need to broadcast anything to anyone even if it's not a direct response for a ws request
