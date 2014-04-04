fdf7df0d419072e30057b9186abb9153ee116bd4
Teoman Soygul <teo@soygul.com>
2014-04-04 22:22:20 +0200

    simplify karma.conf

--- test/client/karma.conf.js
+++ test/client/karma.conf.js
@@ -10,16 +10,10 @@ module.exports = function (config) {
 
     files: [
       'client/bower_components/angular/angular.js',
-      'client/bower_components/**/angular-*.js',
+      'client/bower_components/angular-route/angular-route.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
       'client/js/**/*.js',
       'test/client/unit/**/*.js'
-    ],
-
-    exclude: [
-      'client/bower_components/angular-loader/angular-loader.js',
-      'client/bower_components/**/*.min.js',
-      'client/bower_components/angular-scenario/angular-scenario.js'
     ],
 
     autoWatch: false,
