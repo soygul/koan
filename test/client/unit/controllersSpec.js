f0b8bd7051b84fc2bced40f421763cfcaaf5809d
Teoman Soygul <teo@soygul.com>
2014-04-04 23:13:57 +0200

    add sample unit test

--- test/client/unit/controllersSpec.js
+++ test/client/unit/controllersSpec.js
@@ -2,9 +2,24 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
+describe('controllers', function () {
+
   beforeEach(module('koan.controllers'));
 
+  describe('layout controller', function () {
+    var ctrl, scope, $window, api;
+
+    beforeEach(inject(function ($rootScope, $controller) {
+      scope = $rootScope.$new();
+      $window = {sessionStorage: {user: '{}'}};
+      api = {connected: {subscribe: function () {}}, disconnected: {subscribe: function () {}}};
+      ctrl = $controller('layout', {$scope: scope, $window: $window, $route: {}, api: api});
+    }));
+
+    it('should set defined', function () {
+      expect(ctrl).toBeDefined();
+    });
+  });
 
   it('should ....', inject(function() {
     //spec body
