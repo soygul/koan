fd242a5943930a8afb6e8cac002ca85a73b71b1a
Teoman Soygul <teo@soygul.com>
2014-04-12 18:59:08 +0200

    add chat ctrl

---
+++ client/js/chatbox-controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Controller for managing the chat box functionality shown on the right hand side of the screen.
+ */
+
+angular.module('koan').controller('ChatBoxCtrl', function ($scope, api) {
+});
