1d27cd946ba07b490ec2f5a0a62cf042ae65dbcc
Teoman Soygul <teo@soygul.com>
2014-04-20 03:14:45 +0200

    custom fb login impl

--- server/config/config.js
+++ server/config/config.js
@@ -22,6 +22,13 @@ var platformConfig = {
   development: {
     mongo: {
       url: 'mongodb://localhost:27017/koan-dev'
+    },
+    oauth: {
+      facebook: {
+        clientId: '231235687068678',
+        clientSecret: '4a90381c6bfa738bb18fb7d6046c14b8',
+        callbackUrl: 'http://localhost:3000/login/facebook/callback'
+      }
     }
   },
 
@@ -38,21 +45,11 @@ var platformConfig = {
     app: {
       cacheTime: 7 * 24 * 60 * 60 * 1000 /* default caching time (7 days) for static files, calculated in milliseconds */
     },
-    passport: {
+    oauth: {
       facebook: {
-        clientID: '231235687068678',
+        clientId: '231235687068678',
         clientSecret: '4a90381c6bfa738bb18fb7d6046c14b8',
-        callbackURL: 'http://localhost:3000/login/facebook/callback'
-      },
-      twitter: {
-        clientID: 'CONSUMER_KEY',
-        clientSecret: 'CONSUMER_SECRET',
-        callbackURL: 'http://localhost:3000/login/twitter/callback'
-      },
-      google: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/login/google/callback'
+        callbackUrl: 'http://koanjs.com/login/facebook/callback'
       }
     }
   }
--- server/controllers/public.js
+++ server/controllers/public.js
@@ -7,14 +7,35 @@
 var route = require('koa-route'),
     jwt = require('koa-jwt'),
     parse = require('co-body'),
+    request = require('co-request'),
+    querystring = require('querystring'),
     mongo = require('../config/mongo'),
     config = require('../config/config');
 
 // register koa routes
 exports.init = function (app) {
-  app.use(route.post('/login', login));
   app.use(route.get('/api/users/:id/picture', getPicture));
+  app.use(route.post('/login', login));
+  app.use(route.get('/login/facebook', facebookLogin));
+  app.use(route.get('/login/facebook/callback', facebookCallback));
 };
+
+/**
+ * Serves user profile picture in 50x50 jpeg format.
+ * @param id - User ID.
+ */
+function *getPicture(id) {
+  id = parseInt(id);
+  var user = yield mongo.users.findOne({_id: id}, {picture: 1});
+  if (user) {
+    var img = new Buffer(user.picture, 'base64');
+    this.set('Content-Type', 'image/jpeg');
+    if (config.app.cacheTime) {
+      this.set('Cache-Control', 'max-age=' + (config.app.cacheTime / 1000));
+    }
+    this.body = img;
+  }
+}
 
 /**
  * Retrieves user credentials from an HTML form post (x-www-form-urlencoded) and returns a JSON Web Token along with user profile info in JSON format.
@@ -40,18 +61,54 @@ function *login() {
 }
 
 /**
- * Serves user profile picture in 50x50 jpeg format.
- * @param id User ID.
+ * Facebook OAuth 2.0 login endpoint.
  */
-function *getPicture(id) {
-  id = parseInt(id);
-  var user = yield mongo.users.findOne({_id: id}, {picture: 1});
-  if (user) {
-    var img = new Buffer(user.picture, 'base64');
-    this.set('Content-Type', 'image/jpeg');
-    if (config.app.cacheTime) {
-      this.set('Cache-Control', 'max-age=' + (config.app.cacheTime / 1000));
-    }
-    this.body = img;
+function *facebookLogin() {
+  // todo: implement a stateless nonce algorithm form &state=nonce query param
+  this.redirect(
+          'https://www.facebook.com/dialog/oauth?client_id=' + config.oauth.facebook.clientId +
+          '&redirect_uri=' + config.oauth.facebook.callbackUrl + '&response_type=code&scope=email&state=nonce');
+}
+
+/**
+ * Facebook OAuth 2.0 callback endpoint.
+ */
+function *facebookCallback() {
+  if (this.query.error || this.query.state !== 'nonce') {
+    this.redirect('/login');
+    return;
   }
+
+  // get an access token from facebook in exchange for oauth code
+  var tokenResponse = yield request.get(
+          'https://graph.facebook.com/oauth/access_token?client_id=' + config.oauth.facebook.clientId +
+          '&redirect_uri=' + config.oauth.facebook.callbackUrl +
+          '&client_secret=' + config.oauth.facebook.clientSecret +
+          '&code=' + this.query.code);
+  var token = querystring.parse(tokenResponse.body);
+  if (!token.access_token) {
+    this.redirect('/login');
+    return;
+  }
+
+  // get user profile (including email address) from facebook and save user data in our database if necessary
+  var profileResponse = yield request.get('https://graph.facebook.com/me?fields=name,email,picture.type(large)&access_token=' + token.access_token);
+  var profile = JSON.parse(profileResponse.body);
+  var user = yield mongo.users.findOne({email: profile.email}, {email: 1, name: 1});
+  if (!user) {
+    user = {
+      _id: (yield mongo.getNextSequence('userId')),
+      email: profile.email,
+      name: profile.name,
+      picture: (yield request.get(profile.picture.data.url, {encoding: 'base64'})).body
+    };
+    var results = yield mongo.users.insert(user);
+  }
+
+  // redirect the user to password selection box (for the first timers) or the index page along with user profile object as query string
+  user.id = user._id;
+  delete user.id;
+  user.picture = 'api/users/' + user.id + '/picture';
+  var token = jwt.sign(user, config.app.secret, {expiresInMinutes: 90 * 24 * 60 /* 90 days */});
+  this.redirect('/?user=' + encodeURIComponent(JSON.stringify({token: token, user: user})));
 }
